b267bbb7e24de6166018c11a5ea6d7d7c73a9f2e
unknown <maykedesenvolvimento@gmail.com>
2020-10-21 18:44:07 -0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='center'>
+        <h1>React - Juros compostos</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App

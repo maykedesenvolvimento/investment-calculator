21335925bcf8022e32b07a5acfe37b4d9e5b8205
Mayke Santos <maykesantos@MacBook-Pro.local>
2020-10-22 14:52:47 -0300

    Inputs created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [data, setData] = useState({
+    initial: 0,
+    rate: 0,
+    period: 0
+  })
+
+  const change = (field) => (event) => setData({ ...data, [field]: event.target.value })
+
   return (
     <div className='app'>
       <header className='center'>
         <h1>React - Juros compostos</h1>
       </header>
+      <div className='form'>
+        <div className='form-item'>
+          <label htmlFor='initial'>Montante inicial: </label>
+          <input type='number' id='initial' value={data.initial} onChange={change('initial')} />
+        </div>
+        <div className='form-item'>
+          <label htmlFor='rate'>Taxa de juros (mensal): </label>
+          <input type='number' id='rate' value={data.rate} onChange={change('rate')} />
+        </div>
+        <div className='form-item'>
+          <label htmlFor='period'>Período (em meses): </label>
+          <input type='number' id='period' value={data.period} onChange={change('period')} />
+        </div>
+      </div>
     </div>
   )
 }
